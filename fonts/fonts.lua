1ba2b0a2cefd463f27a48ffb39cff3323a62dfbf
Malevolent_Doctor <malevolentdoctor@gmail.com>
2022-11-08 09:07:22 +1000

    Added new 8x8 font, removed scaled fonts

--- fonts/fonts.lua
+++ fonts/fonts.lua
@@ -1,38 +1,27 @@
 
 Font = {
 	fonts = {
-		["dos8_1"] = love.graphics.newFont("fonts/fnt_ModernDOS8x8.ttf", 16),
-		["dos8_2"] = love.graphics.newFont("fonts/fnt_ModernDOS8x8.ttf", 32),
-		["dos8_3"] = love.graphics.newFont("fonts/fnt_ModernDOS8x8.ttf", 48),
-
-		["dos14_1"] = love.graphics.newFont("fonts/fnt_ModernDOS8x14.ttf", 16),
-		["dos14_2"] = love.graphics.newFont("fonts/fnt_ModernDOS8x14.ttf", 32),
-		["dos14_3"] = love.graphics.newFont("fonts/fnt_ModernDOS8x14.ttf", 48),
-
-		["dos16_1"] = love.graphics.newFont("fonts/fnt_ModernDOS8x16.ttf", 16),
-		["dos16_2"] = love.graphics.newFont("fonts/fnt_ModernDOS8x16.ttf", 32),
-		["dos16_3"] = love.graphics.newFont("fonts/fnt_ModernDOS8x16.ttf", 48),
-
-		["nokia_1"] = love.graphics.newFont("fonts/8x8pixelFont.ttf", 8),
-		["nokia_2"] = love.graphics.newFont("fonts/8x8pixelFont.ttf", 16),
-		["nokia_3"] = love.graphics.newFont("fonts/8x8pixelFont.ttf", 24),
-
-		["start_1"] = love.graphics.newFont("fonts/fnt_pressStart.ttf", 8),
-		["start_2"] = love.graphics.newFont("fonts/fnt_pressStart.ttf", 16),
-		["start_3"] = love.graphics.newFont("fonts/fnt_pressStart.ttf", 24),
-
-		["retro_1"] = love.graphics.newFont("fonts/fnt_retroGaming.ttf", 11),
-		["retro_2"] = love.graphics.newFont("fonts/fnt_retroGaming.ttf", 22),
-		["retro_3"] = love.graphics.newFont("fonts/fnt_retroGaming.ttf", 33),
+		["dos8"] = love.graphics.newFont("fonts/fnt_ModernDOS8x8.ttf", 16, "mono"),
+		["dos14"] = love.graphics.newFont("fonts/fnt_ModernDOS8x14.ttf", 16, "mono"),
+		["dos16"] = love.graphics.newFont("fonts/fnt_ModernDOS8x16.ttf", 16, "mono"),
+		["nokia_mod"] = love.graphics.newFont("fonts/8x8pixelFont.ttf", 8, "mono"),
+		["start"] = love.graphics.newFont("fonts/fnt_pressStart.ttf", 8, "mono"),
+		["retro"] = love.graphics.newFont("fonts/fnt_retroGaming.ttf", 11, "mono"),
 	}
 }
 
+function Font.init()
+	for _,v in pairs(Font.fonts) do
+		v:setFilter("nearest", "nearest", 1)
+	end
+end
 
 function Font:demo()
 	local y = 5;
 	local x = 5;
 	for k,v in pairs(self.fonts) do
-		love.graphics.print(k, v, x, y)
+		love.graphics.setFont(v)
+		love.graphics.print(k, x, y)
 		y = y + v:getHeight() + 2;
 	end
 end
