de4ab5384a5aa427ae8ccaca7f31af01c06537bf
Malevolent_Doctor <malevolentdoctor@gmail.com>
2022-11-08 09:12:43 +1000

    Changed rendering to use a canvas which is scaled

--- main.lua
+++ main.lua
@@ -1,33 +1,41 @@
 require("init")
 
 function love.load()
+	App.init()
+	Font.init()
+
 	love.graphics.setDefaultFilter("nearest", "nearest", 1)
+	love.graphics.setLineStyle("rough")
+
 	love.keyboard.setKeyRepeat(true);
-
-	--print("Program loaded");
-
 	ObjDesktop = Desktop:new();
-	--ObjPIC8 = Micro:new(4, 1, 8, 32);
-	--ObjEditor = TextEditor:new(0, 0, 1280, 720, 20, "edit");
+
 	love.graphics.default_font = love.graphics.getFont();
 end
 
 
+
 function love.update(dt)
 	-- ObjEditor:update();
+	
 	Desktop:update()
 end
 
 
 function love.draw()
 	local window_width, window_height = love.window.getMode()
-	-- ObjEditor:draw();
+
+	love.graphics.setCanvas(App.canvas)
 	ObjDesktop:draw();
 	-- Font:demo()
 
+	love.graphics.setCanvas()
+	love.graphics.setColor(1,1,1)
+	love.graphics.draw(App.canvas, 0, 0, 0, App.scale, App.scale)
+
 	-- debug stuff
 	love.graphics.setFont(love.graphics.default_font)
-	love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), window_width - 110, window_height - 20)
+	love.graphics.print("Current FPS: "..tostring(love.timer.getFPS()), window_width - 110, window_height - 20)
 end
 
 function love.keypressed(key)
@@ -38,7 +46,7 @@ function love.keypressed(key)
 end
 
 function love.mousepressed(x, y, button)
-	mouse.current_key = button;
-	mouse.x = x;
-	mouse.y = y;
+	Mouse.current_key = button;
+	Mouse.x = x;
+	Mouse.y = y;
 end
