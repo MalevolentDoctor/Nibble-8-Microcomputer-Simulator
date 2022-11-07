010cb1472cd35a75e0b6d3ca4d7d20a64cda7557
Malevolent_Doctor <malevolentdoctor@gmail.com>
2022-11-08 09:13:37 +1000

    App table to store program information

---
+++ app.lua
@@ -0,0 +1,20 @@
+App = {
+	scale = 2,
+}
+
+function App.init()
+	App.update()
+	local w, h = love.window.getMode()
+	App.canvas = love.graphics.newCanvas(math.floor(w/2), math.floor(h/2))
+	App.canvas:setFilter("nearest", "nearest", 1)
+end
+
+function App.update()
+	local w, h = love.window.getMode()
+	App.window_width = math.floor(w / App.scale)
+	App.window_height = math.floor(h / App.scale)
+end
+
+function App.getWindowSize()
+	return App.window_width, App.window_height
+end
