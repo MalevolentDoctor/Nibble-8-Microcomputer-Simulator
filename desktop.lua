f8b96b39d2fb17af4879a96065c7d9290df91370
Malevolent_Doctor <malevolentdoctor@gmail.com>
2022-11-08 09:08:44 +1000

    disabled custom mouse

--- desktop.lua
+++ desktop.lua
@@ -2,11 +2,10 @@ Desktop = {};
 
 function Desktop:new()
     setmetatable({}, Desktop)
-    
-    self.header_font_size = 2;             -- size of font in desktop header
-    self.header_font_name = "dos16";       -- name of font used in desktop header
-    self.header_border = 2;
 
+    self.header_font = Font.fonts["dos16"];     -- font (type) used in desktop header
+    self.header_border = 2;
+    
     self.active = true;
 
     -- Colours
@@ -15,34 +14,34 @@ function Desktop:new()
     self.header_text_col = colour.hex("B5A8C6");
 
     -- Calculated values
-    self.header_font = Font.fonts[self.header_font_name .. "_" .. self.header_font_size];   -- font (type) used in desktop header
-    self.header_font_height = self.header_font:getHeight();                                 -- height of font used in desktop header
-    self.header_font_width = self.header_font:getWidth("a");                                -- width (assuming monospaced) of the font used in desktop header
-    self.header_height = self.header_font_height + 2*self.header_border;                    -- height of the header bar
+    
+    self.header_font_height = self.header_font:getHeight();                 -- height of font used in desktop header
+    self.header_font_width = self.header_font:getWidth("a");                -- width (assuming monospaced) of the font used in desktop header
+    self.header_height = self.header_font_height + 2*self.header_border;    -- height of the header bar
 
     -- images
     self.spr_editor_icon = Sprite.new( -- editor icon
-        10, self.header_height + 10, 2, 2, 
+        5, self.header_height + 10, 1, 1, 
         {"assets/png/editor.png", "assets/png/editor_hover.png"},
         {hover = true, visible = false}
     )
     self.btn_editor = Button.new(self.spr_editor_icon, self.openEditor, self)
 
     self.spr_microcomputer_icon = Sprite.new( -- microcontroller icon
-        130, self.header_height + 10, 2, 2, 
+        65, self.header_height + 10, 1, 1, 
         {"assets/png/microcontroller.png", "assets/png/microcontroller_hover.png"},
         {hover = true, visible = false}
     )
     self.btn_mc = Button.new(self.spr_microcomputer_icon, self.openMicrocomputer, self)
 
     self.spr_file_icon = Sprite.new( -- file icon
-        250, self.header_height + 10, 2, 2,
+        125, self.header_height + 10, 1, 1,
         {"assets/png/file.png", "assets/png/file_hover.png"},
         {hover = true}
     )
 
-    self.cursor = love.mouse.newCursor("assets/png/cursor.png", 10, 10);
-    love.mouse.setCursor(self.cursor);
+    --self.cursor = love.mouse.newCursor("assets/png/cursor.png", 10, 10);
+    --love.mouse.setCursor(self.cursor);
 
     self.obj_editor = {};
     self.obj_mcomputer = {};
@@ -52,8 +51,7 @@ function Desktop:new()
 end
 
 function Desktop:update()
-    local window_width, window_height = love.window.getMode()
-    local mx, my = love.mouse.getPosition();
+    local mx, my = Mouse.getPosition();
 
     if self.active == true then
         for _,v in ipairs(Sprite.sprites) do
@@ -62,7 +60,7 @@ function Desktop:update()
             end
         end
 
-        local _,_,m_btn = mouse.getKey()
+        local _,_,m_btn = Mouse.getKey()
         if m_btn == 1 then
             for _,v in ipairs(Button.buttons) do
                 if v:inBB(mx, my) then
@@ -80,11 +78,11 @@ function Desktop:update()
         self.active = true;
     end
 
-    mouse.reset();
+    Mouse.reset();
 end
 
 function Desktop:draw()
-    local window_width, window_height = love.window.getMode()
+    local window_width, window_height = App.getWindowSize()
 
     -- background
     love.graphics.setColor(self.bg_col);
@@ -98,7 +96,8 @@ function Desktop:draw()
     love.graphics.line(0, self.header_height, window_width, self.header_height);
 
     love.graphics.setColor(self.header_text_col)
-    love.graphics.print("Nibble-8", self.header_font, 5, self.header_border)
+    love.graphics.setFont(self.header_font)
+    love.graphics.print("Nibble-8", 5, self.header_border)
 
     -- sprites
     for _,v in pairs(Sprite.sprites) do
@@ -126,10 +125,10 @@ function Desktop:draw()
 end
 
 function Desktop:openEditor()
-    local window_width, window_height = love.window.getMode()
+    local window_width, window_height = App.getWindowSize()
 
     if self.obj_editor.active == nil then
-        self.obj_editor = Editor:new(50, 50, window_width - 100, window_height - 100, "console");
+        self.obj_editor = Editor.new(0, 0, window_width, window_height, "console");
         self.active = false;
         self.spr_editor_icon.i = 1;
     elseif self.obj_editor.active == false then
