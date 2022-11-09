58b82bb775fc9369cfe1c7187fa5e672c5033bab
Malevolent_Doctor <malevolentdoctor@gmail.com>
2022-11-09 17:22:33 +1000

    Added window class for more modularity

--- microcomputer.lua
+++ microcomputer.lua
@@ -10,9 +10,9 @@ function Microcomputer.new(microcontroller)
     self.active = true
 
     -- colours
-    self.col_bg = colour.hex('113F5E')
-    self.col_black = colour.hex('000')
-    self.col_white = colour.hex('fff')
+    self.col_bg = Colour.hex('113F5E')
+    self.col_black = Colour.hex('000')
+    self.col_white = Colour.hex('fff')
 
     -- fonts
     self.fnt_header = Font.fonts["dos14"]
@@ -29,130 +29,112 @@ function Microcomputer.new(microcontroller)
     self.fnt_text_height = self.fnt_text:getHeight();
     self.fnt_text_width = self.fnt_text:getWidth("A");
 
-    do -- registers window parameters
-        self.reg_win = {
-            active = true,
-            x = 0, y = -1,
-            w = -1, h = -1,
-            r = 3,
-            in_buffer = 3, out_buffer = 3,
-
-            -- colours
-            col_bg = colour.hex('4B051F'),
-            col_bdr = colour.hex('000'),
-            col_hdr_bg = colour.hex('380317'),
-            col_hdr_txt = colour.hex('000'),
-
-            -- fonts
-            fnt_hdr = Font.fonts["dos14"],
-            fnt_txt = Font.fonts["pxl_5x7_thin"],
-            fnt_acc = Font.fonts["pxl_5x7_bold"]
-        }
-        
-        -- fonts
-        self.reg_win.fnt_hdr_w = self.reg_win.fnt_hdr:getWidth("A")
-        self.reg_win.fnt_hdr_h = self.reg_win.fnt_hdr:getHeight()
-        self.reg_win.fnt_txt_w = self.reg_win.fnt_txt:getWidth("A")
-        self.reg_win.fnt_txt_h = self.reg_win.fnt_txt:getHeight()
-        self.reg_win.fnt_acc_w = self.reg_win.fnt_acc:getWidth("A")
-        self.reg_win.fnt_acc_h = self.reg_win.fnt_acc:getHeight()
-
-        self.reg_win.fnt_text_yspacing = self.reg_win.fnt_txt_h + 1
-        self.reg_win.fnt_text_hh = math.floor(self.reg_win.fnt_txt_h/2)
+    do -- registers window
+        self.reg_win = Window.new(0, 0, 0, 0, 3, 3, 3, 2, 
+            {"pxl_5x7_thin", "pxl_5x7_bold", "pxl_5x7_thin"},
+            {{'4B051F', '380317'}, {'000'}, {'000'}}
+        )
 
         self.reg_win.reg_rows = math.floor(self.obj_mc.reg_size/2)
-
-        -- window size
-        self.reg_win.h = 2*self.reg_win.out_buffer + 5*self.reg_win.in_buffer + math.floor(6.5*self.reg_win.fnt_text_yspacing) + 2*self.reg_win.fnt_hdr_h - 2
-
-        self.reg_win.y = window_height - self.reg_win.h
-        self.reg_win.hdr_h = self.reg_win.fnt_hdr_h + 2*self.reg_win.in_buffer
-        self.reg_win.w = self.reg_win.fnt_txt_w * 23 + 2*self.reg_win.in_buffer
-
-        -- accounting for the external buffer
-        self.reg_win.x_out = self.reg_win.x + self.reg_win.out_buffer
-        self.reg_win.y_out = self.reg_win.y + self.reg_win.out_buffer
-        self.reg_win.w_out = self.reg_win.w - 2 * self.reg_win.out_buffer
-        self.reg_win.h_out = self.reg_win.h - 2 * self.reg_win.out_buffer
-
-        -- accounting for internal buffer
-        self.reg_win.x_in = self.reg_win.x_out + self.reg_win.in_buffer
-        self.reg_win.y_in = self.reg_win.y_out + self.reg_win.in_buffer
-
-        -- in the body (accounting for the header)
-        self.reg_win.x_body_out = self.reg_win.x_out
-        self.reg_win.y_body_out = self.reg_win.y_out + self.reg_win.hdr_h
-        self.reg_win.x_body_in = self.reg_win.x_body_out + self.reg_win.in_buffer
-        self.reg_win.y_body_in = self.reg_win.y_body_out + self.reg_win.in_buffer
-
-        -- below the accumulator
-        self.reg_win.y_subacc = self.reg_win.y_body_out + self.reg_win.fnt_hdr_h + 1
-        self.reg_win.y_sub_genreg = self.reg_win.y_subacc + self.reg_win.fnt_text_yspacing * self.reg_win.reg_rows + 2*self.reg_win.in_buffer - 1
-
-        self.reg_win.canvas = love.graphics.newCanvas(App.window_width, App.window_height)
+        self.reg_win.registers = self.obj_mc.registers
+        self.reg_win.sp = self.obj_mc.stack_pointer
+        self.reg_win.ip = self.obj_mc.instruction_pointer
 
         -- translation layer
         self.reg_win.trans_reg_ind = {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000"}
         self.reg_win.trans_reg_alpha = {"B", "C", "D", "E", "G", "H", "X", "Y"}
         self.reg_win.trans_xreg_alpha = {"XB", "XD", "XG", " Z"}
 
+        function self.reg_win:draw()
+            self:resetCurrentY()
+            self:drawBackground()
+            self:drawTitle({self.col_txt[1], "REGISTERS"}, true)
+        
+            self:hline(2, self.col_bdr[1])
+            self:printText({self.col_txt[1], " ACCUMULATOR " .. self.registers["0000"]}, self.fnt_txt1)
+            self:vspace(self.int_buffer)
+            self:hline(1, self.col_bdr[1])
+        
+            self:drawBorder(2)
+        
+            -- general purpose registers
+            for i = 1,self.reg_rows do
+                local i2 = 2*i
+                local reg_v1 = self.registers[self.trans_reg_ind[i2 - 1]]
+                local reg_v2 = self.registers[self.trans_reg_ind[i2]]
+                local reg_a1 = self.trans_reg_alpha[i2 - 1]
+                local text
+        
+                if reg_v2 ~= nil then
+                    local reg_a2 = self.trans_reg_alpha[i2]
+                    local xreg = self.trans_xreg_alpha[i]
+                    text = xreg .. " ".. reg_a1  .. reg_v1 .. " " .. reg_a2 .. reg_v1
+                else
+                    text = "   "..reg_a1..reg_v1
+                end
+                self:printText({self.col_txt[1], text}, self.fnt_txt2)
+                self:vspace(1)
+            end
+            self:vspace(self.int_buffer - 1)
+            self:hline(1, self.col_bdr[1])
+        
+            -- -- stack pointer and instruction pointer
+            self:printText({self.col_txt[1], "STACK " .. self.sp}, self.fnt_txt2)
+            self:vspace(1)
+            self:printText({self.col_txt[1], "INSTR " .. self.ip}, self.fnt_txt2)
+        end
+        
+        self.reg_win.h = self.reg_win:getHeight()
+        self.reg_win.y = App.window_height - self.reg_win.h
+        self.reg_win.w = self.reg_win.fnt_txt1_w * 23 + 2*self.reg_win.int_buffer -- check
+
+        self.reg_win:init()
     end
 
-    do -- RAM window parameters
-        self.ram_win = {
-            active = true,
-            x = 0, y = 0,
-            w = -1, h = -1,
-            r = 3,
-            in_buffer = 3, out_buffer = 3,
-
-            -- colours
-            col_bg = colour.hex('4B051F'),
-            col_bdr = colour.hex('000'),
-            col_hdr_bg = colour.hex('380317'),
-            col_hdr_txt = colour.hex('000'),
-
-            -- fonts
-            fnt_hdr = Font.fonts["dos14"],
-            fnt_txt = Font.fonts["pxl_5x7_thin"]
-        }
+    do -- RAM window
+        self.ram_win = Window.new(0, 0, 0, 0, 3, 3, 3, 2,
+        {"pxl_5x7_thin", "pxl_5x7_bold", "pxl_5x7_thin"},
+        {{'4B051F', '380317'}, {'000'}, {'000'}}
+        )
         
-        -- fonts
-        self.ram_win.fnt_hdr_w = self.ram_win.fnt_hdr:getWidth("A")
-        self.ram_win.fnt_hdr_h = self.ram_win.fnt_hdr:getHeight()
-        self.ram_win.fnt_txt_w = self.ram_win.fnt_txt:getWidth("A")
-        self.ram_win.fnt_txt_h = self.ram_win.fnt_txt:getHeight()
-
         self.ram_win.vals_in_row = 8
 
         -- window size
-        self.ram_win.h = window_height - self.reg_win.h + self.ram_win.out_buffer
-        self.ram_win.hdr_h = self.ram_win.fnt_hdr_h + 2*self.ram_win.in_buffer
-        self.ram_win.body_h = self.ram_win.h - self.ram_win.hdr_h
-        self.ram_win.window_rows = math.floor(self.ram_win.body_h/self.ram_win.fnt_txt_h) - 1
-        self.ram_win.row_offset = 0;
-        self.ram_win.char_offset = self.ram_win.row_offset*self.ram_win.vals_in_row;
+        self.ram_win.h = window_height - self.reg_win.h + self.ram_win.ext_buffer
 
         self.ram_win.body_char_width = self.ram_win.vals_in_row * 3 + 6
-        self.ram_win.w = self.ram_win.fnt_txt_w * (self.ram_win.body_char_width + 1) + 2*self.ram_win.in_buffer
+        self.ram_win.w = self.ram_win.fnt_txt2_w * (self.ram_win.body_char_width + 1) + 2*self.ram_win.int_buffer
+        self.ram_win:init()
 
-        -- accounting for the external buffer
-        self.ram_win.x_out = self.ram_win.x + self.ram_win.out_buffer
-        self.ram_win.y_out = self.ram_win.y + self.ram_win.out_buffer
-        self.ram_win.w_out = self.ram_win.w - 2 * self.ram_win.out_buffer
-        self.ram_win.h_out = self.ram_win.h - 2 * self.ram_win.out_buffer
+        self.ram_win.window_rows = math.floor(self.ram_win.h_int/(self.ram_win.fnt_txt2_h + 1)) - 1
+        self.ram_win.row_offset = 0;
 
-        -- accounting for internal buffer
-        self.ram_win.x_in = self.ram_win.x_out + self.ram_win.in_buffer
-        self.ram_win.y_in = self.ram_win.y_out + self.ram_win.in_buffer
+        self.ram_win.ram = self.obj_mc.sram
 
-        -- in the body (accounting for the header)
-        self.ram_win.x_body_out = self.ram_win.x_out
-        self.ram_win.y_body_out = self.ram_win.y_out + self.ram_win.hdr_h
-        self.ram_win.x_body_in = self.ram_win.x_body_out + self.ram_win.in_buffer
-        self.ram_win.y_body_in = self.ram_win.y_body_out + self.ram_win.in_buffer
+        function self.ram_win:draw()
+            self:resetCurrentY()
+            self:drawBackground()
+            self:drawTitle({self.col_txt[1], "RAM"}, true)
+            self:hline(2)
 
-        self.ram_win.canvas = love.graphics.newCanvas(App.window_width, App.window_height)
+            -- ram
+            local vals, val_str
+            for i = 1,self.window_rows do
+                vals = {}
+                local row_index = (i - 1 + self.row_offset)*self.vals_in_row
+                local row_hex_index = bit.tohex(row_index, 4):upper()
+                for j = 1,self.vals_in_row do
+                    local ind = row_index + j - 1;
+                    vals[j] = numbers.byteToHex(self.ram[ind])
+                end
+                val_str = table.concat(vals, " ")
+                self:printText({self.col_txt[1], "0x" .. row_hex_index .. " " .. val_str}, self.fnt_txt2)
+                self:vspace(1)
+            end
+            self:drawBorder(2)
+        end
+
+        self.ram_win:init()
     end
 
     -- sprites
@@ -177,6 +159,14 @@ function Microcomputer:update()
         self.ram_win.char_offset = self.ram_win.row_offset*self.ram_win.vals_in_row;
     end
 
+    for i = 1,1000 do
+        local a = bit.tohex(i,4)
+        local b = bit.tohex(i*2,4)
+        local c = a .. b .. 'h'
+        local d = numbers.toBin(c, 16)
+        print(d)
+    end
+
     keyboard:reset()
 end
 
@@ -189,108 +179,13 @@ function Microcomputer:draw()
     love.graphics.rectangle("fill", 0, 0, window_width, window_height)
 
     -- windows
-    for _ = 1,100 do
-        self:drawRegisters()
-        self:drawRAM()
-        self:drawMicroprocessor()
-    end
+    self.reg_win:draw()
+    self.ram_win:draw()
+    self:drawMicroprocessor()
+
 end
 
 function Microcomputer:drawMC()
-end
-
-function Microcomputer:drawRegisters()
-    --love.graphics.setCanvas(self.wr.canvas) -- (seems to be very slow)
-    -- 47-48 fps no translation, ~55 fps after
-
-    -- background
-    love.graphics.setColor(self.reg_win.col_bg)
-    love.graphics.rectangle("fill", self.reg_win.x_out, self.reg_win.y_out, self.reg_win.w_out, self.reg_win.h_out, self.reg_win.r)
-
-    -- heading
-    love.graphics.setColor(self.reg_win.col_hdr_bg)
-    love.graphics.rectangle("fill", self.reg_win.x_out, self.reg_win.y_out, self.reg_win.w_out, self.reg_win.hdr_h, self.reg_win.r)
-    love.graphics.setColor(self.reg_win.col_hdr_txt)
-    love.graphics.setFont(self.reg_win.fnt_hdr)
-    love.graphics.print("REGISTERS", self.reg_win.x_in, self.reg_win.y_in)
-
-    -- accumulator
-    local acc_text = " ACCUMULATOR "
-    love.graphics.setFont(self.reg_win.fnt_acc)
-    love.graphics.print(acc_text .. self.obj_mc.registers["0000"], self.reg_win.x_body_in, self.reg_win.y_body_in)
-
-    -- general purpose registers
-    love.graphics.setFont(self.reg_win.fnt_txt)
-
-    for i = 1,self.reg_win.reg_rows do
-        local i2 = 2*i
-        local reg_v1 = self.obj_mc.registers[self.reg_win.trans_reg_ind[i2 - 1]]
-        local reg_v2 = self.obj_mc.registers[self.reg_win.trans_reg_ind[i2]]
-        local reg_a1 = self.reg_win.trans_reg_alpha[i2 - 1]
-        local text
-
-        if reg_v2 ~= nil then
-            local reg_a2 = self.reg_win.trans_reg_alpha[i2]
-            local xreg = self.reg_win.trans_xreg_alpha[i]
-            text = xreg .. " ".. reg_a1  .. reg_v1 .. " " .. reg_a2 .. reg_v1
-        else
-            text = "   " .. reg_a1 .. reg_v1
-        end
-        local yy = self.reg_win.y_subacc + self.reg_win.fnt_text_yspacing * (i - 1) + self.reg_win.in_buffer - 1
-        love.graphics.print(text, self.reg_win.x_body_in, yy)
-    end
-
-    -- stack pointer and instruction pointer
-    love.graphics.print("STACK " .. self.obj_mc.stack_pointer, self.reg_win.x_body_in, self.reg_win.y_sub_genreg + self.reg_win.in_buffer - 1)
-    love.graphics.print("INSTR " .. self.obj_mc.instruction_pointer, self.reg_win.x_body_in, self.reg_win.y_sub_genreg + self.reg_win.fnt_text_yspacing + self.reg_win.in_buffer - 1)
-
-    -- border
-    love.graphics.setColor(self.reg_win.col_bdr)
-    love.graphics.setLineWidth(2)
-    love.graphics.rectangle("line", self.reg_win.x_out, self.reg_win.y_out, self.reg_win.w_out, self.reg_win.h_out, self.reg_win.r)
-    love.graphics.line(self.reg_win.x_out, self.reg_win.y_body_out, self.reg_win.x_out + self.reg_win.w_out, self.reg_win.y_body_out)
-    love.graphics.setLineWidth(1)
-    love.graphics.line(self.reg_win.x_out, self.reg_win.y_subacc, self.reg_win.x_out + self.reg_win.w_out, self.reg_win.y_subacc)
-    love.graphics.line(self.reg_win.x_out, self.reg_win.y_sub_genreg, self.reg_win.x_out + self.reg_win.w_out, self.reg_win.y_sub_genreg)
-    
-    --love.graphics.setCanvas(App.canvas)
-end
-
-function Microcomputer:drawRAM()
-    -- background
-    love.graphics.setColor(self.ram_win.col_bg)
-    love.graphics.rectangle("fill", self.ram_win.x_out, self.ram_win.y_out, self.ram_win.w_out, self.ram_win.h_out, self.ram_win.r)
-
-    -- heading
-    love.graphics.setColor(self.ram_win.col_hdr_bg)
-    love.graphics.rectangle("fill", self.ram_win.x_out, self.ram_win.y_out, self.ram_win.w_out, self.ram_win.hdr_h, self.ram_win.r)
-    love.graphics.setColor(self.ram_win.col_hdr_txt)
-    love.graphics.setFont(self.ram_win.fnt_hdr)
-    love.graphics.print("RAM", self.ram_win.x_in, self.ram_win.y_in)
-
-    -- ram
-    love.graphics.setFont(self.ram_win.fnt_txt)
-
-    local vals, val_str, yy
-    for i = 1,self.ram_win.window_rows do
-        vals = {}
-        local row_index = (i - 1 + self.ram_win.row_offset)*self.ram_win.vals_in_row
-        local row_hex_index = bit.tohex(row_index, 4):upper()
-        for j = 1,self.ram_win.vals_in_row do
-            local ind = row_index + j - 1;
-            vals[j] = numbers.byteToHex(self.obj_mc.sram[ind])
-        end
-        val_str = table.concat(vals, " ")
-        yy = self.ram_win.y_body_in + self.ram_win.fnt_txt_h * (i - 1)
-        love.graphics.print("0x" .. row_hex_index .. " " .. val_str, self.ram_win.x_body_in, yy)
-    end
-
-
-    -- border
-    love.graphics.setColor(self.ram_win.col_bdr)
-    love.graphics.setLineWidth(2)
-    love.graphics.rectangle("line", self.ram_win.x_out, self.ram_win.y_out, self.ram_win.w_out, self.ram_win.h_out, self.ram_win.r)
-    love.graphics.line(self.ram_win.x_out, self.ram_win.y_body_out, self.ram_win.x_out + self.ram_win.w_out, self.ram_win.y_body_out)
 end
 
 function Microcomputer:drawRAMTooltip()
@@ -321,8 +216,6 @@ function Microcomputer:drawMicroprocessor()
     love.graphics.print("CARRY:     " .. tostring(self.obj_mc.flag[4]), x + 23, y + 105)
     love.graphics.print("AUX CARRY: " .. tostring(self.obj_mc.flag[5]), x + 23, y + 115)
 end
-
-
 
 function Microcomputer:drawROM()
 end
---
+++ window.lua
@@ -0,0 +1,137 @@
+-- A generic window that can be used for ideally anything
+Window = {}
+Window.__index = Window
+
+-- set the parameters for the window (semi-constant)
+function Window.new(x, y, w, h, r, exbuff, inbuff, bdr, fonts, colours)
+    local self = {}
+    setmetatable(self, Window)
+
+    self.active = true
+
+    -- window size and shape
+    self.x = x or 0
+    self.y = y or 0
+    self.w = w or 0
+    self.h = h or 0
+    self.r = r or 0
+
+    self.ext_buffer = exbuff or 0
+    self.int_buffer = inbuff or 0
+
+    self.border = bdr or 0
+
+    -- fonts {1: header, 2: text 1, 3 text 2, ...}
+    self.fnt_hdr, self.fnt_hdr_w, self.fnt_hdr_h = Font.getFont(fonts[1])
+    self.fnt_txt1, self.fnt_txt1_w, self.fnt_txt1_h = Font.getFont(fonts[2])
+    self.fnt_txt2, self.fnt_txt2_w, self.fnt_txt2_h = Font.getFont(fonts[3])
+
+    -- colours {{background}, {border}, {text}}
+    self.col_bg = {}
+    self.col_bdr = {}
+    self.col_txt = {}
+    if colours ~= nil then
+        if colours[1] ~= nil then
+            for i,v in ipairs(colours[1]) do
+                self.col_bg[i] = Colour.hex(v)
+            end
+        end
+        if colours[2] ~= nil then
+            for i,v in ipairs(colours[2]) do
+                self.col_bdr[i] = Colour.hex(v)
+            end
+        end
+        if colours[3] ~= nil then
+            for i,v in ipairs(colours[3]) do
+                self.col_txt[i] = Colour.hex(v)
+            end
+        end
+    end
+    
+
+    return self
+end
+
+-- calculate once anything that relies on the initial parameters (can be called again to update)
+function Window:init()
+    self.x_ext = self.x + self.ext_buffer
+    self.y_ext = self.y + self.ext_buffer
+    self.w_ext = self.w - 2 * self.ext_buffer
+    self.h_ext = self.h - 2 * self.ext_buffer
+
+    self.x_int = self.x_ext + self.int_buffer
+    self.y_int = self.y_ext + self.int_buffer
+    self.w_int = self.w_ext - 2 * self.int_buffer
+    self.h_int = self.h_ext - 2 * self.int_buffer
+
+    self.current_y = self.y_int
+    self.reset_y = self.y_int
+
+    -- precalculate to optimise
+    self.hdr_h = self.int_buffer * 2 + self.fnt_hdr_h + self.border
+    self.hdr_y_offset = self.int_buffer + self.fnt_hdr_h + self.border
+
+    
+end
+
+-- draws a horizontal line of specified width and colour at the current y value
+function Window:hline(width, colour)
+	local col = colour or self.col_bdr[1]
+    self.current_y = self.current_y - math.ceil(width/2)
+    love.graphics.setLineWidth(0)
+    love.graphics.setColor(col)
+    love.graphics.rectangle("fill", self.x_ext, self.current_y, self.w_ext, width)
+    self.current_y = self.current_y + width + self.int_buffer + 1
+end
+
+-- prints text, either table or string, of specified colour at current y and x
+function Window:printText(text, font, x)
+    love.graphics.setFont(font)
+    x = x or self.x_int
+    love.graphics.print(text, x, self.current_y)
+    self.current_y = self.current_y + font:getHeight()
+end
+
+-- adds a vertical space
+function Window:vspace(px)
+    self.current_y = self.current_y + px
+end
+
+function Window:drawBackground()
+    love.graphics.setColor(self.col_bg[1])
+    love.graphics.rectangle("fill", self.x_ext, self.y_ext, self.w_ext, self.h_ext, self.r)
+end
+
+function Window:drawBorder(width)
+    love.graphics.setColor(self.col_bdr[1])
+    love.graphics.setLineWidth(width)
+    love.graphics.rectangle("line", self.x_ext, self.y_ext, self.w_ext, self.h_ext, self.r)
+end
+
+function Window:drawTitle(text, background)
+    if background then
+        love.graphics.setColor(self.col_bg[2])
+        love.graphics.rectangle("fill", self.x_ext, self.y_ext, self.w_ext, self.hdr_h)
+    end
+    love.graphics.setFont(self.fnt_hdr)
+    love.graphics.print(text, self.x_int, self.current_y + self.border)
+    self.current_y = self.current_y + self.hdr_y_offset
+end
+
+function Window:resetCurrentY()
+    self.current_y = self.reset_y
+end
+
+function Window:draw()
+end
+
+function Window:getHeight()
+    local parent_canvas = love.graphics.getCanvas()
+    local temp_canvas = love.graphics.newCanvas(1,1)
+    love.graphics.setCanvas(temp_canvas)
+    self:init()
+    self:draw()
+    love.graphics.setCanvas(parent_canvas)
+
+    return self.current_y + self.ext_buffer + self.int_buffer
+end
