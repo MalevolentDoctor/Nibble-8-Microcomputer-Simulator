5be3918e4dda449c0a0cae9bbcbd18d0f0870bf9
Malevolent_Doctor <malevolentdoctor@gmail.com>
2022-11-19 13:23:43 +1000

    Adjusted default screen size parameters

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
 	t.window.title = "8-bit Microcontroller"
-	t.window.height = 800;
-	t.window.width = 1280;
+	t.window.height = 720; -- 360, 720, 1080, 1440, 1800, 2160
+	t.window.width = 1280; -- 640, 1280, 1920, 2560, 3200, 3840
 	t.console = false;
 	t.window.msaa = 0;
 	t.window.vsync = false;
