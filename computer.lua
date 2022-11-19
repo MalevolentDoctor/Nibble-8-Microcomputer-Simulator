c19d90ebba144a69eaffd86cc7aaac8d1ecf37a3
Malevolent_Doctor <malevolentdoctor@gmail.com>
2022-11-19 13:27:43 +1000

    added new, update and draw functions

--- computer.lua
+++ computer.lua
@@ -5,12 +5,29 @@ function Computer.new()
 	local self = {}
 	setmetatable(self, Computer)
 
+	self.objConsole = Console.new(self, 0, 0, App.window_width - 200, App.window_height - 40, true)
+	self.canvas_screen = love.graphics.newCanvas(self.objConsole.w + 2, self.objConsole.h + 2)
+	-- self.canvas_screen:setFilter("linear", "linear", 1)
+
+	self.crt = love.graphics.newShader("shaders/fragment/frag_crt.glsl", "shaders/vertex/vert_passthrough.glsl")
+
 	return self
 end
 
 
 function Computer:update()
+	self.objConsole:update()
 end
 
 function Computer:draw()
+	love.graphics.setCanvas(self.canvas_screen)
+	self.objConsole:draw()
+	love.graphics.reset()
+
+	love.graphics.setShader(self.crt)
+	self.crt:send("window_width", App.window_width)
+	self.crt:send("window_scale", App.scale)
+	love.graphics.draw(self.canvas_screen, 100, 20, 0, App.scale, App.scale)
+
+	love.graphics.reset()
 end
--- workbench.lua
+++ workbench.lua
@@ -2,13 +2,37 @@ Workbench = {}
 Workbench.__index = Workbench
 
 function Workbench.new()
-	self = {}
+	local self = {}
 	setmetatable(self, Workbench)
+
+	self.obj_computer = Computer.new() -- object of the desktop (your PC) screen
+	self.obj_microcomputer = Microcomputer.new() -- object containing the microcomputer
+
+	self.page = 0
+
+	self.crt = love.graphics.newShader("shaders/fragment/frag_crt.glsl", "shaders/vertex/vert_passthrough.glsl")
+	self.spr_workbench_pg1_bg = Sprite.new(0, 0, App.scale, App.scale, {"assets/png/workbench_pg1.png"})
+
 	return self
 end
 
 function Workbench:update()
+	local fun_key = keyboard:getFunKey();
+
+	-- switch between the pages using function keys
+	if fun_key == "f1" then self.page = 0 end
+	if fun_key == "f2" then self.page = 1 end
+
+	if self.page == 0 then
+		-- if on the first page we update the desktop screen on the computer
+		self.obj_computer:update()
+	end
 end
 
 function Workbench:draw()
+	if self.page == 0 then
+		-- if on the first page we draw the desktop screen on the computer
+		self.obj_computer:draw()
+		self.spr_workbench_pg1_bg:draw()
+	end
 end
