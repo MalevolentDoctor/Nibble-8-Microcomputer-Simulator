b1fcf67850bac836df7c5ad7ee95ded22e737c16
Malevolent_Doctor <malevolentdoctor@gmail.com>
2022-12-04 12:52:39 +1000

    lots of minor changes

--- microcomputer.lua
+++ microcomputer.lua
@@ -9,41 +9,29 @@ function Microcomputer.new()
 
     self.active = true
 
-    -- colours
-    self.col_bg = Colour.hex('113F5E')
-    self.col_black = Colour.hex('000')
-    self.col_white = Colour.hex('fff')
+    self.obj_microprocessor = Microprocessor.new(self,8);
+    self.address_bus = 0x0000
+    self.data_bus = 0x00
 
-    -- fonts
-    self.fnt_header = Font.fonts["dos14"]
-    self.fnt_text = Font.fonts["dos14"]
+    self.ram_size = 0x0100 -- 4kb ram
+    self.rom_size = 0x0100 -- 4kb rom
 
+    self.obj_ram = Ram.new(self, 0, self.ram_size)
+    self.obj_rom = Rom.new(self, 0xFFFF - self.rom_size + 1, self.rom_size)
+
+    -- AESTHETIC PARAMETERS
     local _, window_height = App.getWindowSize()
-
-    self.obj_microcontroller = Microcontroller.new(1,1,1,1);
-
-    -- calculated values
-    self.fnt_header_height = self.fnt_header:getHeight();
-    self.fnt_header_width = self.fnt_header:getWidth("A");
-
-    self.fnt_text_height = self.fnt_text:getHeight();
-    self.fnt_text_width = self.fnt_text:getWidth("A");
-
     do -- registers window
-        self.reg_win = Window.new(0, 0, 0, 0, 3, 3, 3, 2, 
+        self.reg_win = Window.new(self.obj_microprocessor, 0, 0, 0, 0, 3, 3, 3, 2, 
             {"pxl_5x7_thin", "pxl_5x7_bold", "pxl_5x7_thin"},
-            {{'4B051F', '380317'}, {'000'}, {'000'}}
+            {{'4B051F', '380317', '000'}, {'000'}, {'000'}}
         )
 
-        self.reg_win.reg_rows = math.floor(self.obj_microcontroller.reg_size/2)
-        self.reg_win.registers = self.obj_microcontroller.registers
-        self.reg_win.sp = self.obj_microcontroller.stack_pointer
-        self.reg_win.ip = self.obj_microcontroller.instruction_pointer
+        self.reg_win.reg_rows = math.ceil(self.obj_microprocessor.reg_size/2)
 
         -- translation layer
-        self.reg_win.trans_reg_ind = {"0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000"}
         self.reg_win.trans_reg_alpha = {"B", "C", "D", "E", "G", "H", "X", "Y"}
-        self.reg_win.trans_xreg_alpha = {"XB", "XD", "XG", " Z"}
+        self.reg_win.trans_xreg_alpha = {"BX", "DX", "GX", " Z"}
 
         function self.reg_win:draw()
             self:resetCurrentY()
@@ -51,7 +39,7 @@ function Microcomputer.new()
             self:drawTitle({self.col_txt[1], "REGISTERS"}, true)
         
             self:hline(2, self.col_bdr[1])
-            self:printText({self.col_txt[1], " ACCUMULATOR " .. self.registers["0000"]}, self.fnt_txt1)
+            self:printText({self.col_txt[1], " ACCUMULATOR " .. numbers.toBin(self.other.accumulator, 8)}, self.fnt_txt1)
             self:vspace(self.int_buffer)
             self:hline(1, self.col_bdr[1])
         
@@ -59,32 +47,29 @@ function Microcomputer.new()
         
             -- general purpose registers
             for i = 1,self.reg_rows do
-                local i2 = 2*i
-                local reg_v1 = self.registers[self.trans_reg_ind[i2 - 1]]
-                local reg_v2 = self.registers[self.trans_reg_ind[i2]]
-                local reg_a1 = self.trans_reg_alpha[i2 - 1]
+                local reg_v1 = numbers.toBin(self.other.gp_registers[2*i - 1], 8)
+                local reg_v2 = numbers.toBin(self.other.gp_registers[2*i], 8)
+                local reg_a1 = self.trans_reg_alpha[2*i - 1]
                 local text
         
                 if reg_v2 ~= nil then
-                    local reg_a2 = self.trans_reg_alpha[i2]
+                    local reg_a2 = self.trans_reg_alpha[2*i]
                     local xreg = self.trans_xreg_alpha[i]
-                    text = xreg .. " ".. reg_a1  .. reg_v1 .. " " .. reg_a2 .. reg_v1
+                    text = xreg .. " ".. reg_a1  .. reg_v1 .. " " .. reg_a2 .. reg_v2
                 else
                     text = "   "..reg_a1..reg_v1
                 end
                 self:printText({self.col_txt[1], text}, self.fnt_txt2)
-                self:vspace(1)
             end
             self:vspace(self.int_buffer - 1)
             self:hline(1, self.col_bdr[1])
         
             -- -- stack pointer and instruction pointer
-            self:printText({self.col_txt[1], "STACK " .. self.sp}, self.fnt_txt2)
-            self:vspace(1)
-            self:printText({self.col_txt[1], "INSTR " .. self.ip}, self.fnt_txt2)
+            self:printText({self.col_txt[1], "STACK " .. numbers.toBin(self.other.stack_pointer, 16)}, self.fnt_txt2)
+            self:printText({self.col_txt[1], "INSTR " .. numbers.toBin(self.other.instruction_pointer, 16)}, self.fnt_txt2)
         end
         
-        self.reg_win.h = self.reg_win:getHeight()
+        self.reg_win.h = self.reg_win:getHeight(self.obj_microprocessor)
         self.reg_win.y = App.window_height - self.reg_win.h
         self.reg_win.w = self.reg_win.fnt_txt1_w * 23 + 2*self.reg_win.int_buffer -- check
 
@@ -92,7 +77,7 @@ function Microcomputer.new()
     end
 
     do -- RAM window
-        self.ram_win = Window.new(0, 0, 0, 0, 3, 3, 3, 2,
+        self.ram_win = Window.new(self.obj_ram, 0, 0, 0, 0, 3, 3, 3, 2,
         {"pxl_5x7_thin", "pxl_5x7_bold", "pxl_5x7_thin"},
         {{'4B051F', '380317'}, {'000'}, {'000'}}
         )
@@ -106,10 +91,8 @@ function Microcomputer.new()
         self.ram_win.w = self.ram_win.fnt_txt2_w * (self.ram_win.body_char_width + 1) + 2*self.ram_win.int_buffer
         self.ram_win:init()
 
-        self.ram_win.window_rows = math.floor(self.ram_win.h_int/(self.ram_win.fnt_txt2_h + 1)) - 1
+        self.ram_win.window_rows = math.floor((self.ram_win.h_int - self.ram_win.hdr_h)/(self.ram_win.fnt_txt2_h + self.ram_win.line_spacing))
         self.ram_win.row_offset = 0;
-
-        self.ram_win.ram = self.obj_microcontroller.sram
 
         function self.ram_win:draw()
             self:resetCurrentY()
@@ -125,7 +108,7 @@ function Microcomputer.new()
                 local row_hex_index = bit.tohex(row_index, 4):upper()
                 for j = 1,self.vals_in_row do
                     local ind = row_index + j - 1;
-                    vals[j] = numbers.byteToHex(self.ram[ind])
+                    vals[j] = numbers.byteToHex(self.other.ram[ind])
                 end
                 val_str = table.concat(vals, " ")
                 self:printText({self.col_txt[1], "0x" .. row_hex_index .. " " .. val_str}, self.fnt_txt2)
@@ -137,23 +120,23 @@ function Microcomputer.new()
     end
 
     -- sprites
-    self.spr_microcontroller = Sprite.new(
+    self.spr_microprocessor = Sprite.new(
         -1, 10, 1, 1, 
         {"assets/png/microcontroller_chip.png"}, "nearest",
         {hover = false, visible = false}
     )
-    self.spr_microcontroller.x = math.floor(App.window_width/2) - math.floor(self.spr_microcontroller.w/2)
+    self.spr_microprocessor.x = math.floor(App.window_width/2) - math.floor(self.spr_microprocessor.w/2)
 
     return self
 end
 
-function Microcomputer:update()
+function Microcomputer:update(dt)
     if self.active then
         local nav_key = keyboard:getNavKey()
         local edit_key = keyboard:getEditKey()
 
         if nav_key == "pagedown" then
-            self.ram_win.row_offset = math.min(math.ceil(self.obj_mc.sram_size/self.ram_win.vals_in_row) - self.ram_win.window_rows, self.ram_win.row_offset + self.ram_win.window_rows - 1);
+            self.ram_win.row_offset = math.min(math.ceil(self.obj_ram.size/self.ram_win.vals_in_row) - self.ram_win.window_rows, self.ram_win.row_offset + self.ram_win.window_rows - 1);
             self.ram_win.char_offset = self.ram_win.row_offset*self.ram_win.vals_in_row;
         elseif nav_key == "pageup" then
             self.ram_win.row_offset = math.max(0, self.ram_win.row_offset - self.ram_win.window_rows + 1);
@@ -164,18 +147,19 @@ function Microcomputer:update()
             self.active = false
         end
 
+        if nav_key == "home" then
+            self.obj_microprocessor:start()
+            self.obj_microprocessor.active = true
+        end
+
+        self.obj_microprocessor:update(dt)
+
         keyboard:reset()
     end
 end
 
 function Microcomputer:draw()
     if self.active then
-        local window_width, window_height = App.getWindowSize()
-        love.graphics.setFont(self.fnt_text)
-
-        -- background
-        love.graphics.setColor(self.col_bg);
-        love.graphics.rectangle("fill", 0, 0, window_width, window_height)
 
         -- windows
         self.reg_win:draw()
@@ -192,32 +176,47 @@ end
 
 function Microcomputer:drawMicroprocessor()
     love.graphics.setColor(1,1,1)
-    self.spr_microcontroller:draw()
+    self.spr_microprocessor:draw()
 
-    local x = self.spr_microcontroller.x
-    local y = self.spr_microcontroller.y
-    local w = self.spr_microcontroller.w
-    local h = self.spr_microcontroller.h
+    local x = self.spr_microprocessor.x
+    local y = self.spr_microprocessor.y
+    local w = self.spr_microprocessor.w
+    local h = self.spr_microprocessor.h
 
     love.graphics.setColor(0,0,0)
     love.graphics.setFont(Font.fonts["pxl_5x7_bold"])
     love.graphics.print("NIBBLE-8", x + 23, y + 7)
     love.graphics.print("FLAGS", x + 23, y + 65)
     love.graphics.setFont(Font.fonts["pxl_5x7_thin"])
-    love.graphics.print("GP REGISTERS:" .. tostring(self.obj_microcontroller.reg_size - 1), x + 23, y + 20)
-    love.graphics.print("RAM:" .. tostring(self.obj_microcontroller.sram_size) .. " Bytes", x + 23, y + 30)
-    love.graphics.print("ROM:" .. tostring(self.obj_microcontroller.flash_size) .. " Bytes", x + 23, y + 40)
-    love.graphics.print("IO:" .. tostring(self.obj_microcontroller.io_size) .. " Ports", x + 23, y + 50)
+    love.graphics.print("GP REGISTERS:" .. tostring(self.obj_microprocessor.reg_size), x + 23, y + 20)
+    love.graphics.print("RAM:" .. tostring(self.obj_ram.size) .. " Bytes", x + 23, y + 30)
+    love.graphics.print("ROM:" .. tostring(self.obj_rom.size) .. " Bytes", x + 23, y + 40)
+    love.graphics.print("IO:" .. tostring("NaN") .. " Ports", x + 23, y + 50)
 
-    love.graphics.print("SIGN:      " .. tostring(self.obj_microcontroller.flag[1]), x + 23, y + 75)
-    love.graphics.print("ZERO:      " .. tostring(self.obj_microcontroller.flag[2]), x + 23, y + 85)
-    love.graphics.print("PARITY:    " .. tostring(self.obj_microcontroller.flag[3]), x + 23, y + 95)
-    love.graphics.print("CARRY:     " .. tostring(self.obj_microcontroller.flag[4]), x + 23, y + 105)
-    love.graphics.print("AUX CARRY: " .. tostring(self.obj_microcontroller.flag[5]), x + 23, y + 115)
+    love.graphics.print("SIGN:      " .. tostring(self.obj_microprocessor:getSign()), x + 23, y + 75)
+    love.graphics.print("ZERO:      " .. tostring(self.obj_microprocessor:getZero()), x + 23, y + 85)
+    love.graphics.print("PARITY:    " .. tostring(self.obj_microprocessor:getParity()), x + 23, y + 95)
+    love.graphics.print("CARRY:     " .. tostring(self.obj_microprocessor:getCarry()), x + 23, y + 105)
+    love.graphics.print("INTRPT:    " .. tostring(self.obj_microprocessor:getInterruptDisable()), x + 23, y + 115)
 end
 
 function Microcomputer:drawROM()
 end
 
 function Microcomputer:drawIO()
+end
+
+function Microcomputer:readAddressPins()
+    self.address_bus = self.obj_microprocessor.address_bus
+end
+
+function Microcomputer:readMemory()
+    self:readAddressPins()
+    self.data_bus = self.obj_ram.ram[self.address_bus] or self.obj_rom.rom[self.address_bus]
+end
+
+function Microcomputer:writeMemory()
+    if self.obj_ram.ram[self.address_bus] ~= nil then
+        self.obj_ram.ram[self.address_bus] = self.data_bus
+    end
 end
